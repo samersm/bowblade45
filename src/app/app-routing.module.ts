587375784edf683078bd0e526dddf8dea6f6b653
Samer Zagzoog <mrsamer.dev@gmail.com>
2020-05-25 22:08:15 +0300

    Added Bootstrap, Components & Root Path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { BloodborneComponent } from './pages/bloodborne/bloodborne.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './pages/navbar/navbar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BloodborneComponent },
+  { path: 'navbar', component: NavbarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BloodborneComponent } from './pages/bloodborne/bloodborne.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BloodborneComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
